f53f5f81acbeba39040848c801f7f5eea5758b33
zhxu33 <77419802+zhxu33@users.noreply.github.com>
2023-05-20 15:01:38 -0700

    Revert "backend fix"

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,7 @@
 import React from "react";
-import Input from "./Input.js"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Input />}/>
-      </Routes>
-    </Router>
-  );
+  return <div></div>;
 }
 
 export default App;
