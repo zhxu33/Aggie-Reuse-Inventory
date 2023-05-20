f4d172a4b9372cc8fd8315ddeeffc047be4e2f3e
Kuangzhong Niu <kuangzhongniu@gmail.com>
2023-05-20 16:34:19 -0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import './App.css'
 
 function App() {
-  return <div>hello <h1>hi</h1></div>;
+  let items = [{name: "T-shirt1", description: "blue"}, {name: "T-shirt2", description: "green"}, {name: "T-shirt3", description: "blue"}]
+  return <div class="flex-container">
+  {items.map((item) => (
+        <div>
+          <h10>{item.name}</h10>
+          <h5>{item.description}</h5>
+          </div>
+      ))}
+  <div></div>
+  <div></div>
+  <div></div>  
+  <div></div>
+  <div></div>  
+  <div></div>
+  
+
+  
+</div>
 }
 
 export default App;
