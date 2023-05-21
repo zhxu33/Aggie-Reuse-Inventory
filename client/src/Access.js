e15d10901cb90c77eda3f47196d326fde1754721
Tam Ray <raytam@Tams-MacBook-Air.local>
2023-05-20 21:30:58 -0700

    added accordions

--- client/src/Access.js
+++ client/src/Access.js
@@ -88,6 +88,60 @@ const Access = () => {
         </Grid>
       </Grid>
     </div>
+
+import React, { useState } from 'react';
+import { Accordion, AccordionSummary, AccordionDetails, Typography, Button } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const a = Array.from({ length: 100 }, (_, index) => index + 1);
+const elementsPerPage = 10;
+
+const PageWithAccordions = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const startIndex = (currentPage - 1) * elementsPerPage;
+  const endIndex = startIndex + elementsPerPage;
+  const visibleElements = a.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(a.length / elementsPerPage);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <div>
+      {visibleElements.map((element) => (
+        <Accordion key={element}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography>{element}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Accordion Content for {element}
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+
+      <div>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <Button
+            key={index + 1}
+            variant={currentPage === index + 1 ? 'contained' : 'outlined'}
+            color="primary"
+            onClick={() => handlePageChange(index + 1)}
+          >
+            {index + 1}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PageWithAccordions;
+
   );
 }
 
