02e993ff00dcfa186a5d2184c66fe9d70441b3f4
zhxu33 <77419802+zhxu33@users.noreply.github.com>
2023-08-23 20:16:42 -0700

    deploy update

--- server/server.js
+++ server/server.js
@@ -3,14 +3,6 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const cors = require("cors");
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
 app.use(express.json());
 
 const itemsRouter = require("./routes/items");
